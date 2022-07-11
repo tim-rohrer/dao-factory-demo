fddbf8eb89b4cf5f0f6d22d586dd79adb7279d08
tim.rohrer <tim@tgr66.dev>
2022-07-10 21:08:02 -0400

    test: Add jest and memory-server setup.

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,19 @@
-module.exports = {
-  roots: ["<rootDir>/src"],
-  transform: {
-    "^.+.tsx?$": "ts-jest",
+const config = {
+  globalSetup: "./test/setup.ts",
+  globalTeardown: "./test/teardown.ts",
+  verbose: true,
+  extensionsToTreatAsEsm: [".ts"],
+  globals: {
+    "ts-jest": {
+      useESM: true,
+    },
   },
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  preset: "ts-jest/presets/default-esm",
+  rootDir: "src",
+  transform: {},
 }
+
+export default config
